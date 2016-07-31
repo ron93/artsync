bdecdfa2b03adf32607e53142206da5ef1d431bc
ron93 <ronmkare1@gmail.com>
2016-07-31 10:46:22 +0300

    header moved bottom

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -1,19 +1,11 @@
-/**
- * cbpAnimatedHeader.js v1.0.0
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2013, Codrops
- * http://www.codrops.com
- */
+//animated header..onscroll shrinkeffect
+
 var AnimatedHeader = (function() {
 
 	var docElem = document.documentElement,
 		header = document.querySelector( '.header' ),
 		didScroll = false,
-		changeHeaderOn = 200;
+		changeHeaderOn = 100;
 
 	function init() {
 		window.addEventListener( 'scroll', function( event ) {
